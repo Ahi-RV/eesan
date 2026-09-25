63e53b963c9abaf54cd07208a71b6e449d70b882
Ahi-RV <Ahirv2026@outlook.com>
2026-09-25 08:29:07 +0800

    Add test/graph.test.js for EESAN Stage 1

---
+++ test/graph.test.js
@@ -0,0 +1,7 @@
+const test = require('node:test');
+const assert = require('node:assert/strict');
+const { LIBRARY_PATHS } = require('../src/graph');
+test('Stage 1 targets the five agreed library locations', () => {
+  assert.deepEqual(LIBRARY_PATHS, ['Manhole/Duct', 'Manhole/Card', 'SLD', 'Termination', 'Planmap']);
+});
+
