4da13dc6840851645179d59adae020d78d836951
Ahi-RV <Ahirv2026@outlook.com>
2026-09-25 08:50:09 +0800

    Support one central EESAN PDF library

--- test/graph.test.js
+++ test/graph.test.js
@@ -1,7 +1,7 @@
 const test = require('node:test');
 const assert = require('node:assert/strict');
-const { LIBRARY_PATHS } = require('../src/graph');
-test('Stage 1 targets the five agreed library locations', () => {
-  assert.deepEqual(LIBRARY_PATHS, ['Manhole/Duct', 'Manhole/Card', 'SLD', 'Termination', 'Planmap']);
+const { GraphClient } = require('../src/graph');
+test('Stage 1 exposes the OneDrive library traversal client', () => {
+  assert.equal(typeof GraphClient.prototype.discoverPdfs, 'function');
 });
 
